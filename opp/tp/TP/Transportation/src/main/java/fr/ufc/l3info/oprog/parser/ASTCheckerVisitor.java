c9daef56b176bd7437fdfc97f68a44208d9dace0
juliengauthier410 <typ490@gmail.com>
2023-10-24 17:38:23 +0200

    save 24-oct-2023_1738

--- opp/tp/TP/Transportation/src/main/java/fr/ufc/l3info/oprog/parser/ASTCheckerVisitor.java
+++ opp/tp/TP/Transportation/src/main/java/fr/ufc/l3info/oprog/parser/ASTCheckerVisitor.java
@@ -1,16 +1,88 @@
 package fr.ufc.l3info.oprog.parser;
 
-public interface ASTCheckerVisitor {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-    public Object visit(ASTNode n);
+public class ASTCheckerVisitor implements ASTNodeVisitor {
 
-    public Object visit(ASTListeLignes n);
+    private final Map<String, ERROR_KIND> errors = new HashMap<>();
+    private final Set<String> lineNames = new HashSet<>();
+    private final Map<String, Set<String>> stationsPerLine = new HashMap<>();
+    private double lastNumber = -1;
 
-    public Object visit(ASTLigne n);
+    public Map<String, ERROR_KIND> getErrors() {
+        return new HashMap<>(errors);
+    }
 
-    public Object visit(ASTDeclaration n);
+    @Override
+    public Object visit(ASTNode n) {
+        return null;
+    }
 
-    public Object visit(ASTChaine n);
+    @Override
+    public Object visit(ASTListeLignes n) {
+        if (n.getNumChildren() == 0) {
+            errors.put("ListeLignes", ERROR_KIND.EMPTY_LINE_LIST);
+        }
+        return null;
+    }
 
-    public Object visit(ASTNombre n);
+    @Override
+    public Object visit(ASTLigne n) {
+        String lineName = n.value.trim();
+        if (lineName.isEmpty()) {
+            errors.put("Ligne", ERROR_KIND.EMPTY_LINE_NAME);
+        } else if (lineNames.contains(lineName)) {
+            errors.put("Ligne", ERROR_KIND.DUPLICATE_LINE_NAME);
+        } else {
+            lineNames.add(lineName);
+            stationsPerLine.put(lineName, new HashSet<>());
+        }
+        lastNumber = -1;  // Reset for each new line
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTDeclaration n) {
+        String lineName = n.getChild(0).value.trim();  // Assuming the line name is the first child
+        String stationName = n.getChild(1).value.trim();  // Assuming the station name is the second child
+        double number = ((ASTNombre) n.getChild(2)).getNumberValue();  // Assuming the number is the third child
+
+        if (stationName.isEmpty()) {
+            errors.put("Declaration", ERROR_KIND.EMPTY_STATION_NAME);
+        } else if (stationsPerLine.get(lineName).contains(stationName)) {
+            errors.put("Declaration", ERROR_KIND.DUPLICATE_STATION_NAME);
+        } else {
+            stationsPerLine.get(lineName).add(stationName);
+        }
+
+        if (number <= lastNumber || number < 0) {
+            errors.put("Declaration", ERROR_KIND.WRONG_NUMBER_VALUE);
+        }
+        lastNumber = number;
+
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTChaine n) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ASTNombre n) {
+        return null;
+    }
+}
+
+enum ERROR_KIND {
+    EMPTY_LINE_LIST,
+    EMPTY_LINE_NAME,
+    DUPLICATE_LINE_NAME,
+    EMPTY_STATION_NAME,
+    DUPLICATE_STATION_NAME,
+    WRONG_NUMBER_VALUE,
+    UNREACHABLE_LINE
 }
