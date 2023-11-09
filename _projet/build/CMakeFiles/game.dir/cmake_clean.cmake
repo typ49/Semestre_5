0a2c3eff675b5037b6d4a3c83d6f6377489ee84f
typ49 <typ490@gmail.com>
2023-11-09 22:08:27 +0100

    save 09-nov-2023_2208

---
+++ _projet/build/CMakeFiles/game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game.dir/home/juliengauthier410/l3/s5/_projet/data/Input.cc.o"
+  "CMakeFiles/game.dir/home/juliengauthier410/l3/s5/_projet/data/main.cc.o"
+  "CMakeFiles/game.dir/home/juliengauthier410/l3/s5/_projet/data/squareEntity.cc.o"
+  "CMakeFiles/game.dir/home/juliengauthier410/l3/s5/_projet/data/staticPlateforme.cc.o"
+  "game"
+  "game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
