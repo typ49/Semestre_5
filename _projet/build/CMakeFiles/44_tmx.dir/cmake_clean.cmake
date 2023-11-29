068e4c7da5f89f1e8e19cd7fca771e6b51cc7acc
typ49 <typ490@gmail.com>
2023-11-29 11:03:19 +0100

    save 29-nov-2023_1103

---
+++ _projet/build/CMakeFiles/44_tmx.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "44_tmx"
+  "44_tmx.pdb"
+  "CMakeFiles/44_tmx.dir/data/44_tmx.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/44_tmx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _projet/build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/data/44_tmx.cc.o"
+  "example"
+  "example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
